1fca77485bff8e3be652a319091dea9696a36676
edururiuma <edururiumasaisankar@gmail.com>
2024-09-09 15:42:04 +0530

    WAP to find the length of the string using recursion.

--- strlen_recursion.c
+++ strlen_recursion.c
@@ -8,6 +8,6 @@ int len_recursion(char *a){
 void main(int argc,char *argv[]){
 	int l;
 	l=len_recursion(argv[1]);
-	printf("length of the string(argv[1]) = %d\n",l);
+	printf("length of the string(%s) = %d\n",argv[1],l);
 }
 
