75f8017f6d9386d41e3b965aaff063de10cb02c1
edururiuma <edururiumasaisankar@gmail.com>
2024-09-18 16:18:39 +0530

    unwanted libraries

--- library/dynamic_sorting_lib/sorting.h
+++
@@ -1,5 +0,0 @@
-#ifndef _SORTING_H_
-#define _SORTING_H_
-void bubble_sort(int a[],int l);
-void selection_sort(int a[],int l);
-#endif
--- library/static_sorting_lib/sorting.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SORTING_H
-#define _SORTING_H
-void bubble_sort(int a[],int l);
-void selection_sort(int a[],int l);
-#endif
-
