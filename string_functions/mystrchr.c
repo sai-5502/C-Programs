bd5abb3bea368bbc8e60cca3951ab47a5c54c2fc
edururiuma <edururiumasaisankar@gmail.com>
2024-10-04 11:49:29 +0530

    Implementation of my own strchr().

---
+++ string_functions/mystrchr.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+char *my_strchr(char *str,char ch){
+	while (*str){
+		if (*str == ch)
+			return str;
+		else
+			str++;
+	}
+	return NULL;
+}
+
+void main(){
+	char string[]="uma sai sankar";
+	char *p=NULL;
+	char ch='s';
+	p=my_strchr(string,ch);
+	
+	if (p==NULL)
+		printf("The character (%c) is not present in the given string (%s)\n",ch,string);
+	else{
+		printf("%s\n",p);
+		printf("The character (%c) is present at %ld position in the string (%s)\n",ch,p-string,string);
+	}
+}
